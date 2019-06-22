3689b286f88740daf92ef54f9d5947149d1e1956
Steve Ayerhart <steve@ayerh.art>
2019-06-21 22:34:10 -0500

    stream-info mostly done

--- src/flac/metadata.scm
+++ src/flac/metadata.scm
@@ -1,25 +1,100 @@
 (define-module (flac metadata)
   #:use-module (flac config)
-  #:use-module (flac bindings)
-  #:use-module (flac format)
 
   #:use-module (oop goops)
   #:use-module (system foreign-object)
-  #:use-module (system foreign)
+  #:use-module (rnrs bytevectors)
   #:use-module (rnrs enums)
-  #:use-module (rnrs bytevectors))
-
-(define (get-stream-info path)
-  (let ((stream-info (make <stream-info>)))
-    (if (= 1 (%metadata_get_streaminfo (string->pointer path)
-                                       (bytevector->pointer (slot-ref stream-info 'instance))))
-        stream-info
-        (error "NOOOOO"))))
 
-(define (get-tags path)
-  (let* ((vorbis-comment (make <vorbis-comment>))
-        (tags-pointer (scm->pointer (bytevector->pointer (slot-ref vorbis-comment 'instance)))))
-    (if (= 1 (%metadata_get_tags (string->pointer path)
-                                 tags-pointer))
-        (values vorbis-comment tags-pointer (dereference-pointer tags-pointer))
-        (error "NOO"))))
+  #:export (<stream-info>
+            min-block-size
+            max-block-size
+            min-frame-size
+            max-frame-size
+            sample-rate
+            channels
+            bits-per-sample
+            total-samples
+            md5sum))
+
+(define-class <stream-metadata-class> (<class>)
+  (type #:class <protected-slot>))
+
+(define-class <stream-metadata> ()
+  (instance #:class <read-only-slot>)
+  #:metaclass <stream-metadata-class>)
+
+(eval-when (eval load compile)
+  (dynamic-call "guile_flac_metadata_init"
+                (dynamic-link (extensions-library "flac"))))
+
+(eval-when (eval load)
+  (%protect-stream-metadata-class! <stream-metadata>))
+
+(define-method (initialize (self <stream-metadata-class>) args)
+  (%initialize-stream-metadata-class! self args)
+  (next-method))
+
+(define-method (allocate-instance (self <stream-metadata-class>) args)
+  (let ((instance (next-method)))
+    (%allocate-stream-metadata-class self instance)))
+
+(define-class <stream-info> (<stream-metadata>)
+  (min-block-size #:allocation #:virtual
+                  #:accessor min-block-size
+                  #:slot-ref %stream-info-get-min-block-size
+                  #:slot-set! (λ (a b) #f))
+
+  (max-block-size #:allocation #:virtual
+                  #:accessor max-block-size
+                  #:slot-ref %stream-info-get-max-block-size
+                  #:slot-set! (λ (a b) #f))
+
+  (min-frame-size #:allocation #:virtual
+                  #:accessor min-frame-size
+                  #:slot-ref %stream-info-get-min-frame-size
+                  #:slot-set! (λ (a b) #f))
+
+  (max-frame-size #:allocation #:virtual
+                  #:accessor max-frame-size
+                  #:slot-ref %stream-info-get-max-frame-size
+                  #:slot-set! (λ (a b) #f))
+
+  (sample-rate #:allocation #:virtual
+               #:accessor sample-rate
+               #:slot-ref %stream-info-get-sample-rate
+               #:slot-set! (λ (a b) #f))
+
+  (channels #:allocation #:virtual
+               #:accessor sample-rate
+               #:slot-ref %stream-info-get-sample-rate
+               #:slot-set! (λ (a b) #f))
+
+  (bits-per-sample #:allocation #:virtual
+               #:accessor bits-per-sample
+               #:slot-ref %stream-info-get-bits-per-sample
+               #:slot-set! (λ (a b) #f))
+
+  (total-samples #:allocation #:virtual
+               #:accessor total-samples
+               #:slot-ref %stream-info-get-total-samples
+               #:slot-set! (λ (a b) #f))
+
+  (md5sum #:allocation #:virtual
+          #:accessor md5sum
+          #:slot-ref (λ (stream-info)
+                       (string-concatenate
+                        (map (λ (index)
+                               (format #f "~2,'0x" (bytevector-u8-ref
+                                                    (%stream-info-get-md5sum stream-info)
+                                                    index)))
+                             (iota 16))))
+          #:slot-set! (λ (a b) #f))
+
+
+  #:metaclass <stream-metadata-class>
+  #:type 0)
+
+(define-class <vorbis-comment> (<stream-metadata>)
+  #:metaclass <stream-metadata-class>
+  #:type 4)
--- src/metadata.c
+++ src/metadata.c
@@ -23,27 +23,186 @@
   (define subset-max-rice-partition-order 8)
 */
 
-SCM_DEFINE(scm_allocate_flac_metadata, "%allocate-flac-metadata!", 1, 0, 0,
-           (SCM scm_metadata, SCM scm_metadata_type),
+static SCM scm_make_instance;
+
+// helper macros
+
+#define FLAC_METADATA_GET_INSTANCE(class) (FLAC__StreamMetadata *) SCM_STRUCT_DATA_REF (class, 0)
+
+SCM_GLOBAL_SYMBOL (scm_flac_sym_type, "type");
+
+SCM_KEYWORD (scm_flac_kw_type, "type");
+
+static void
+scm_flac_metadata_struct_free (SCM scm_flac_metadata)
+{
+  FLAC__StreamMetadata *stream_metadata;
+
+  stream_metadata = (FLAC__StreamMetadata *) SCM_STRUCT_DATA_REF (scm_flac_metadata, 0);
+
+  if (stream_metadata)
+    {
+      FLAC__metadata_object_delete (stream_metadata);
+      // TODO: call scm_gc_free?
+    }
+}
+
+SCM_DEFINE (scm_protect_stream_metadata_class, "%protect-stream-metadata-class!", 1, 0, 0,
+            (SCM scm_metadata_class),
+            "")
+{
+  scm_t_bits *slots = SCM_STRUCT_DATA (scm_metadata_class);
+  SCM scm_class_metadata_class = scm_permanent_object (scm_variable_ref (scm_c_lookup ("<stream-metadata>")));
+
+  slots[scm_vtable_index_instance_finalize] = (scm_t_bits) scm_flac_metadata_struct_free;
+
+  return SCM_UNSPECIFIED;
+}
+
+SCM_DEFINE (scm_initialize_stream_metadata_class, "%initialize-stream-metadata-class!", 2, 0, 0,
+           (SCM scm_metadata_class, SCM scm_args),
+           "")
+{
+  SCM scm_type = SCM_UNDEFINED;
+
+  scm_c_bind_keyword_arguments ("initialize", scm_args, SCM_ALLOW_OTHER_KEYS,
+                                scm_flac_kw_type, &scm_type,
+                                SCM_UNDEFINED);
+
+  if (SCM_UNBNDP (scm_type))
+    scm_misc_error ("initialize", "fart", SCM_EOL);
+
+  scm_slot_set_x (scm_metadata_class, scm_flac_sym_type, scm_type);
+
+  return SCM_UNSPECIFIED;
+}
+
+SCM_DEFINE (scm_allocate_flac_metadata, "%allocate-stream-metadata-class", 2, 0, 0,
+           (SCM scm_metadata_class, SCM scm_metadata_instance),
            "")
 {
   FLAC__StreamMetadata *stream_metadata;
   FLAC__MetadataType metadata_type;
 
-  metadata_type = (FLAC__MetadataType) scm_to_int (scm_metadata_type);
+  metadata_type = scm_to_int (scm_slot_ref (scm_metadata_class, scm_flac_sym_type));
   stream_metadata = FLAC__metadata_object_new (metadata_type);
 
-  scm_foreign_object_set_x (scm_metadata_class, 0, (void *) stream_metadata);
+  SCM_STRUCT_DATA (scm_metadata_instance)[0] = (scm_t_bits) stream_metadata;
 
-  return scm_metadata;
+  return scm_metadata_instance;
 }
 
-SCM_DEFINE(scm_finalize_flac_metadata, "%finalize-flac-metadata!", 1, 0, 0,
-           (SCM scm_class),
-           "")
+// STREAMINFO
+
+SCM_DEFINE (scm_stream_info_get_min_blocksize, "%stream-info-get-min-block-size", 1, 0, 0,
+            (SCM scm_streaminfo),
+            "")
 {
-  return SCM_UNDEFINED;
+  FLAC__StreamMetadata *stream_metadata = FLAC_METADATA_GET_INSTANCE (scm_streaminfo);
+  return scm_from_uint (stream_metadata->data.stream_info.min_blocksize);
 }
+
+SCM_DEFINE (scm_stream_info_get_max_blocksize, "%stream-info-get-max-block-size", 1, 0, 0,
+            (SCM scm_streaminfo),
+            "")
+{
+  FLAC__StreamMetadata *stream_metadata = FLAC_METADATA_GET_INSTANCE (scm_streaminfo);
+  return scm_from_uint (stream_metadata->data.stream_info.max_blocksize);
+}
+
+SCM_DEFINE (scm_stream_info_get_min_framesize, "%stream-info-get-min-frame-size", 1, 0, 0,
+            (SCM scm_streaminfo),
+            "")
+{
+  FLAC__StreamMetadata *stream_metadata = FLAC_METADATA_GET_INSTANCE (scm_streaminfo);
+  return scm_from_uint (stream_metadata->data.stream_info.min_framesize);
+}
+
+SCM_DEFINE (scm_stream_info_get_max_framesize, "%stream-info-get-max-frame-size", 1, 0, 0,
+            (SCM scm_streaminfo),
+            "")
+{
+  FLAC__StreamMetadata *stream_metadata = FLAC_METADATA_GET_INSTANCE (scm_streaminfo);
+  return scm_from_uint (stream_metadata->data.stream_info.max_framesize);
+}
+
+SCM_DEFINE (scm_stream_info_get_sample_rate, "%stream-info-get-sample-rate", 1, 0, 0,
+            (SCM scm_streaminfo),
+            "")
+{
+  FLAC__StreamMetadata *stream_metadata = FLAC_METADATA_GET_INSTANCE (scm_streaminfo);
+  return scm_from_uint (stream_metadata->data.stream_info.sample_rate);
+}
+
+SCM_DEFINE (scm_stream_info_get_channels, "%stream-info-get-channels", 1, 0, 0,
+            (SCM scm_streaminfo),
+            "")
+{
+  FLAC__StreamMetadata *stream_metadata = FLAC_METADATA_GET_INSTANCE (scm_streaminfo);
+  return scm_from_uint (stream_metadata->data.stream_info.channels);
+}
+
+SCM_DEFINE (scm_stream_info_get_bits_per_sample, "%stream-info-get-bits-per-sample", 1, 0, 0,
+            (SCM scm_streaminfo),
+            "")
+{
+  FLAC__StreamMetadata *stream_metadata = FLAC_METADATA_GET_INSTANCE (scm_streaminfo);
+  return scm_from_uint (stream_metadata->data.stream_info.bits_per_sample);
+}
+
+SCM_DEFINE (scm_stream_info_get_total_sample, "%stream-info-get-total-samples", 1, 0, 0,
+            (SCM scm_streaminfo),
+            "")
+{
+  FLAC__StreamMetadata *stream_metadata = FLAC_METADATA_GET_INSTANCE (scm_streaminfo);
+  return scm_from_uint64 (stream_metadata->data.stream_info.total_samples);
+}
+
+SCM_DEFINE (scm_stream_info_get_md5sum, "%stream-info-get-md5sum", 1, 0, 0,
+            (SCM scm_streaminfo),
+            "")
+{
+  FLAC__StreamMetadata *stream_metadata = FLAC_METADATA_GET_INSTANCE (scm_streaminfo);
+  return scm_pointer_to_bytevector (scm_from_pointer ((void *) stream_metadata->data.stream_info.md5sum, NULL),
+                                    scm_from_uint(16),
+                                    scm_from_uint(0),
+                                    scm_from_utf8_symbol ("u8"));
+}
+
+SCM_DEFINE (scm_metadata_get_streaminfo, "%flac-metadata-get-stream-info", 1, 0, 0,
+            (SCM scm_path),
+            "")
+{
+  FLAC__StreamMetadata *stream_metadata;
+  FLAC__bool status;
+  char *path;
+  SCM scm_stream_info_instance;
+
+  scm_dynwind_begin (0);
+
+  scm_stream_info_instance = scm_call_1 (scm_make_instance,
+                                         scm_variable_ref (scm_c_lookup ("<stream-info>")));
+  stream_metadata = (FLAC__StreamMetadata *) SCM_STRUCT_DATA_REF (scm_stream_info_instance, 0);
+  path = scm_to_locale_string (scm_path);
+
+  scm_dynwind_free (path);
+
+  status = FLAC__metadata_get_streaminfo (path, stream_metadata);
+
+  scm_dynwind_end ();
+
+  if (status == false)
+    scm_error (scm_from_locale_symbol ("memory-allocation-error"),
+               NULL,
+               NULL,
+               SCM_EOL, SCM_EOL);
+
+
+  SCM_STRUCT_DATA (scm_stream_info_instance)[0] = (scm_t_bits) stream_metadata;
+
+  return scm_stream_info_instance;
+}
+
 
 void
 guile_flac_metadata_init (void)
@@ -55,8 +214,12 @@ guile_flac_metadata_init (void)
   scm_c_define ("flac-version", scm_from_locale_string (FLAC__VERSION_STRING));
   scm_c_define ("flac-vendor", scm_from_locale_string (FLAC__VENDOR_STRING));
 
-
   scm_c_export ("flac-version",
                 "flac-vendor",
                 NULL);
+
+  // (oop goops)
+  scm_make_instance = scm_permanent_object (scm_variable_ref (scm_c_lookup ("make-instance")));
+
+
 }
